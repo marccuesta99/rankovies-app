430a088a52af0eee9e9f7475fe781a718d0a881e
marccuesta99 <61467000+marccuesta99@users.noreply.github.com>
2024-01-23 09:17:07 +0100

    mini fix

--- src/app/[type]/components/footer.tsx
+++ src/app/[type]/components/footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => (
   <footer className="absolute w-100 bottom-0 p-4 bg-gray-200 dark:bg-gray-700 text-gray-700 dark:text-gray-300">
     <div className="container mx-auto">
       <p className="text-center">
-        &copy; 2024 Your Movie App. All rights reserved.
+        &copy; 2023-2024 Your Movie App. All rights reserved.
       </p>
     </div>
   </footer>
